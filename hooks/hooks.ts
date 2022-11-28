9e66dadbb3b210c3b4db645d657bed33c4e429dd
Damian Bermudez <bermudezdamian7gmail.com>
2022-11-28 19:52:15 -0300

    solucionando problemas de cors

--- hooks/hooks.ts
+++ hooks/hooks.ts
@@ -2,13 +2,17 @@ import { getSettedToken } from "lib/api";
 import { useEffect, useState } from "react";
 import {
   atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+  useSetRecoilState,
 } from "recoil";
 import { recoilPersist } from "recoil-persist";
 
 const { persistAtom } = recoilPersist();
 
 export const userMailState = atom({
-  key: "stateUserMail",
+  key: "userMailState",
   default: "",
   effects_UNSTABLE: [persistAtom],
 });
