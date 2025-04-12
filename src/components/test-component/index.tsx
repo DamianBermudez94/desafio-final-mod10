c32bbfbe1e240690fe851c47c9285b4c36e955df
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-04-12 18:53:12 -0300

    ..

---
+++ src/components/test-component/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const TestComponent = () => {
+    return (
+        <div className="w-[200px] h-[200px] bg-blue-500 text-white p-4">
+            <p>Este es un test de Tailwind CSS</p>
+        </div>
+    )
+}
