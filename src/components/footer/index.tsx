25c67bcc5d743f632a92912dbad4fbdada2f8178
DamianBermudez94 <bermudezdamian7@gamil.com>
2025-04-28 23:08:57 -0300

    se agregaron iconos al footer

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -2,14 +2,26 @@ import React from "react";
 import { ShoppingCart } from "lucide-react";
 import { Facebook } from "lucide-react";
 import { Instagram } from "lucide-react";
+import { PhoneForwarded } from "lucide-react";
+import { Mail } from "lucide-react";
+import { MapPinCheckInside } from "lucide-react";
 export const Footer: React.FC = () => {
   return (
     <footer className="flex flex-col items-center justify-between w-full h-auto bg-neutral-100">
       <div className="flex items-center justify-around w-full gap-5 mt-4">
         <div className="">
-          <p>shoppinonline@gmail.com</p>
-          <p>+11-4453-455</p>
-          <p>Av Indepencia N°1234</p>
+          <p className="flex gap-2 pt-1">
+            <Mail /> shoppinonline@gmail.com
+          </p>
+
+          <p className="flex gap-2 pt-2">
+            {" "}
+            <PhoneForwarded /> +11-4453-455
+          </p>
+          <p className="flex gap-2 pt-2">
+            <MapPinCheckInside />
+            Av Indepencia N°1234
+          </p>
         </div>
         <div className="flex flex-col justify-between">
           <h4 className="text-2xl font-bold">Nuestras redes</h4>
@@ -23,7 +35,8 @@ export const Footer: React.FC = () => {
           </div>
         </div>
       </div>
-      <div className="flex p-5 m-3 border-t-1 gap-9">
+      <hr className="h-[1px] caret-amber-200 m-3" />
+      <div className="flex p-5 m-3 gap-9">
         <a href="">
           <ShoppingCart />
         </a>
